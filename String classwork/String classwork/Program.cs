c3dfee33e3f86451e45fd0c76a8623c5d473ab7e
Samirkhgcode <138569634+Samirkhgcode@users.noreply.github.com>
2023-07-20 00:29:42 +0400

    String

---
+++ String classwork/String classwork/Program.cs
@@ -0,0 +1,176 @@
+﻿
+
+using String_classwork;
+
+
+//static string ReverseStr(string str)
+//{
+//    string result = "";
+
+//     for (int i =str.Length - 1; i >= 0; i--)
+//    {
+//        result += str[i];   
+//    }
+
+//    return result;
+//}
+
+//Console.WriteLine(ReverseStr("salam"));
+
+//------------------------------//
+
+
+// static void Search(string searchText)
+//{
+
+//  string[] people = { "Kubra", "Fidan", "Samir", "Ismayil" };
+
+//    foreach (string item in people)
+//    {
+//        if (item.Trim().ToLower() == searchText.ToLower().Trim())
+//        {
+//            Console.WriteLine(item);
+//            return;
+//        }
+//    }
+//}
+
+//Search("   kubra  ");
+
+
+
+
+//--------------------------------------//
+
+
+
+
+
+
+//Student stu1 = new Student();
+
+//stu1.name = "Ismayil";
+//stu1.surname = "Efendiyev";
+//stu1.age = 29;
+//stu1.address = "Yasamal";
+
+
+//Student stu2 = new Student();
+//stu2.name = "Xeyyam";
+//stu2.surname = "Isgenderov";
+//stu2.age = 29;
+//stu2.address = "Yasamal";
+
+//Student stu3 = new()
+//{
+//    name = "Kubra",
+//    surname = "Memmedova",
+//    age = 25,
+//    address = "Xalqlar"
+
+//};
+
+
+
+//Student[] = students {stu1, stu2, stu3};
+
+//foreach (var item in students)
+//{
+//    Console.WriteLine(item.name + " " + item.surname);
+//}
+
+
+//---------------------------------//
+
+
+
+
+
+//Book book1 = new()
+//{
+//    name = "Xosrov ve Shirin",
+//    author = "Nizami",
+
+//};
+
+//Console.WriteLine(book1.name + " " + book1.author);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
